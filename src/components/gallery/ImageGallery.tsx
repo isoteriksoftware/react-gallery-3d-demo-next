59a5be49b4828f227c07b3067bf014e9026cb931
imran <isoteriksoftware@gmail.com>
2024-06-04 22:15:59 +0100

    updated image gallery

--- src/components/gallery/ImageGallery.tsx
+++ src/components/gallery/ImageGallery.tsx
@@ -1,17 +1,28 @@
 import { ImageItem } from "react-gallery-3d";
 import DemoScene from "@/components/gallery/DemoScene";
+import { useTexture } from "@react-three/drei";
 
-const ImageGallery = () => {
+const Renderer = () => {
   const images = Array.from(
     { length: 6 },
     (_, i) => `./images/img${i + 1}.jpg`,
   );
 
+  const textures = useTexture(images);
+
+  return (
+    <>
+      {textures.map((texture, index) => (
+        <ImageItem key={index} texture={texture} />
+      ))}
+    </>
+  );
+};
+
+const ImageGallery = () => {
   return (
     <DemoScene>
-      {images.map((image, index) => (
-        <ImageItem key={index} src={image} />
-      ))}
+      <Renderer />
     </DemoScene>
   );
 };
